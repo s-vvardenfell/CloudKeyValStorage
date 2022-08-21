5b0b26afa9acfb3f44050051d8c7c8b4debe263a
s.vvardenfell <s.vvardenfell@yandex.ru>
2022-08-21 19:04:47 +0300

    simple routing

--- main.go
+++ main.go
@@ -1,5 +1,80 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"io"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+	"github.com/s-vvardenfell/CloudKeyValStorage/storage"
+)
+
+// keyValuePutHandler ожидает получить PUT-запрос с
+// ресурсом "/v1/key/{key}".
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	// Получить ключ из запроса
+	key := vars["key"]
+	value, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	// Тело запроса хранит значение
+	if err != nil {
+		// Если возникла ошибка, сообщить о ней
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+	err = storage.Put(key, string(value))
+	// Сохранить значение как строку
+	if err != nil {
+		// Если возникла ошибка, сообщить о ней
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated) // Все хорошо! Вернуть StatusCreated
+}
+
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	// Извлечь ключ из запроса
+	key := vars["key"]
+	value, err := storage.Get(key)
+	// Получить значение для данного ключа
+	if errors.Is(err, storage.ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte(value))
+	// Записать значение в ответ
+}
+
+func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	// Извлечь ключ из запроса
+	key := vars["key"]
+	err := storage.Delete(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	r := mux.NewRouter()
+	// Зарегистрировать keyValuePutHandler как обработчик HTTP-запросов PUT,
+	// в которых указан путь "/v1/{key}"
+	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
+	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
+	r.HandleFunc("/v1/{key}", keyValueDeleteHandler).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
