20e7fc31a2fff8f035eb62e672fcd96f6764645c
s.vvardenfell <s.vvardenfell@yandex.ru>
2022-08-22 16:48:15 +0300

    file journal in progress

---
+++ file_journal/event.go
@@ -0,0 +1,16 @@
+package file_journal
+
+type EventType byte
+
+const (
+	_                     = iota // iota == 0; игнорировать нулевое значение
+	EventDelete EventType = iota // iota == 1
+	EventPut                     // iota == 2; неявное присваивание
+)
+
+type Event struct {
+	Sequence  uint64    // Уникальный порядковый номер записи
+	EventType EventType // Выполненное действие
+	Key       string    // Ключ, затронутый этой транзакцией
+	Value     string    // Значение для транзакции PUT
+}
