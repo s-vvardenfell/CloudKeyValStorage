911ef80b8f06fc49bfd97f63421dc0764589c5be
s.vvardenfell <s.vvardenfell@yandex.ru>
2022-08-24 14:01:59 +0300

    upd logger

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CloudKeyValStorage
+
+curl -X PUT -d 'Val 1' -v http://localhost:8080/v1/key-1
+curl -X GET -v http://localhost:8080/v1/key-1
+curl -X DELETE -v http://localhost:8080/v1/key-1
--- main.go
+++ main.go
@@ -2,13 +2,45 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	fj "github.com/s-vvardenfell/CloudKeyValStorage/file_journal"
 	"github.com/s-vvardenfell/CloudKeyValStorage/storage"
 )
+
+var logger fj.TransactionLogger
+
+func initializeTransactionLog() error {
+	var err error
+	logger, err = fj.NewFileTransactionLogger("logs/transaction.log")
+	if err != nil {
+		return fmt.Errorf("failed to create event logger: %w", err)
+	}
+
+	events, errors := logger.ReadEvents()
+	e, ok := fj.Event{}, true
+
+	for ok && err == nil {
+		select {
+		case err, ok = <-errors: // Получает ошибки
+		case e, ok = <-events:
+			switch e.EventType {
+			case fj.EventDelete:
+				// Получено событие DELETE!
+				err = storage.Delete(e.Key)
+			case fj.EventPut:
+				// Получено событие PUT!
+				err = storage.Put(e.Key, e.Value)
+			}
+		}
+	}
+	logger.Run()
+	return err
+}
 
 // keyValuePutHandler ожидает получить PUT-запрос с
 // ресурсом "/v1/key/{key}".
@@ -26,6 +58,7 @@ func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
 			http.StatusInternalServerError)
 		return
 	}
+
 	err = storage.Put(key, string(value))
 	// Сохранить значение как строку
 	if err != nil {
@@ -35,6 +68,8 @@ func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
 			http.StatusInternalServerError)
 		return
 	}
+
+	logger.WritePut(key, string(value))
 	w.WriteHeader(http.StatusCreated) // Все хорошо! Вернуть StatusCreated
 }
 
@@ -60,15 +95,20 @@ func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	// Извлечь ключ из запроса
 	key := vars["key"]
+
 	err := storage.Delete(key)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	logger.WriteDelete(key)
 	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
+	initializeTransactionLog()
+
 	r := mux.NewRouter()
 	// Зарегистрировать keyValuePutHandler как обработчик HTTP-запросов PUT,
 	// в которых указан путь "/v1/{key}"
